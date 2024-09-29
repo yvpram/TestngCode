dd16485932fc47dc413392fdf99fd1995348426e
varaprasadram1 <prasad.ram.vara.y@gmail.com>
2024-09-29 08:03:29 +0530

    adding annotations pakages for (class level, method level, varilble level) and those relevented imp[Clemented class, We can see all are implemented in one class that is NewAnnotationsCMV class.

---
+++ src/main/java/annotationsCMV/NewAnnotationsMethod.java
@@ -0,0 +1,14 @@
+package annotationsCMV;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NewAnnotationsMethod {
+	public String collge();
+	public String section();
+
+}
